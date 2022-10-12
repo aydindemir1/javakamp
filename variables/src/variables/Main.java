f9d3ecfb4a623b4b585d3971a3efd1a0189fe0b5
aydindemir1 <aynndemr@gmail.com>
2022-10-12 18:55:02 +0300

    https://github.com/aydindemir1/javakamp.git

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 12;
+		String mesaj = "öğrenci sayısı : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("öğrenci sayım : " + ogrenciSayisi);
+		System.out.println("öğrenci sayım : " + ogrenciSayisi);
+
+	}
+
+}
