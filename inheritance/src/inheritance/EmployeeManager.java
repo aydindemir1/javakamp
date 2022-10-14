b5e72511f59a9e091df457c953c09092b88eeb8f
aydindemir1 <aynndemr@gmail.com>
2022-10-14 18:14:08 +0300

    https://github.com/aydindemir1/javakamp.git

--- inheritance/src/inheritance/EmployeeManager.java
+++ inheritance/src/inheritance/EmployeeManager.java
@@ -2,8 +2,6 @@ package inheritance;
 
 public class EmployeeManager extends PersonManager {
 	
-	
-
 	public void bestEmployee() {
 		System.out.println("ayın elemanı getirildi.");
 	}
---
+++ interfaces/src/interfaces/MySqlCustomerDal.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public class MySqlCustomerDal implements ICustomerDal, IRepository {
+
+	@Override
+	public void add() {
+		System.out.println("My Sql eklendi");
+		
+	}
+
+	
+}
